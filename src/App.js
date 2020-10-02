beb30cdf8142fed2853563f84cb0168aea8b3493
a12989x <alexis12989x@gmail.com>
2020-10-02 12:37:07 -0500

    Import Logo, InfoCards and NewsCards and display dynamic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import InfoCards from './components/InfoCards,';
+import NewsCards from './components/NewsCards';
+
+import Logo from './img/Logo.svg';
+
+const { REACT_APP_ALAN_KEY } = process.env;
 
 const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]);
+
+    useEffect(() => {
+        alanBtn({
+            key: REACT_APP_ALAN_KEY,
+            onCommand: ({ command, articles }) => {
+                if (command === 'newHeadlines') {
+                    setNewsArticles(articles);
+                }
+            },
+        });
+    }, []);
+
     return (
         <div className="App">
-            <h1>App Component</h1>
+            <header className="header">
+                <img
+                    src={Logo}
+                    alt="Alan Assistant Logo"
+                    className="header__img"
+                />
+                <h1 className="header__title">Ai News Application</h1>
+            </header>
+            {newsArticles.length === 0 ? (
+                <InfoCards />
+            ) : (
+                <NewsCards articles={newsArticles} />
+            )}
         </div>
     );
 };
