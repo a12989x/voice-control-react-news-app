04bd884c80cb88f7a14a519808e27b02834b8ce5
a12989x <alexis12989x@gmail.com>
2020-10-05 18:09:02 -0500

    Add commands and pass activeArticle in props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from 'words-to-numbers';
 
 import InfoCards from './components/InfoCards,';
 import NewsCards from './components/NewsCards';
@@ -10,13 +11,33 @@ const { REACT_APP_ALAN_KEY } = process.env;
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
 
     useEffect(() => {
         alanBtn({
             key: REACT_APP_ALAN_KEY,
-            onCommand: ({ command, articles }) => {
+            onCommand: ({ command, articles, number }) => {
                 if (command === 'newHeadlines') {
                     setNewsArticles(articles);
+                    setActiveArticle(-1);
+                } else if (command === 'highlight') {
+                    setActiveArticle(
+                        (prevActiveArticle) => prevActiveArticle + 1
+                    );
+                } else if (command === 'open') {
+                    const parsedNumber =
+                        number.length > 2
+                            ? wordsToNumbers(number, { fuzzy: true })
+                            : number;
+
+                    const article = articles[parsedNumber];
+
+                    if (parsedNumber > 20)
+                        alanBtn().playText('Please try that again.');
+                    else if (article) {
+                        window.open(article.url, '_blank');
+                        alanBtn().playText('Opening...');
+                    }
                 }
             },
         });
@@ -35,7 +56,10 @@ const App = () => {
             {newsArticles.length === 0 ? (
                 <InfoCards />
             ) : (
-                <NewsCards articles={newsArticles} />
+                <NewsCards
+                    articles={newsArticles}
+                    activeArticle={activeArticle}
+                />
             )}
         </div>
     );
